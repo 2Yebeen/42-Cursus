923fcd7eb234d234a08c375805c5690f5831c6fe
yeblee <68188768+33bini@users.noreply.github.com>
2022-08-02 16:31:20 +0900

    UPDATE : main

--- push_swap/srcs/main.c
+++ push_swap/srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: yeblee <yeblee@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/02 15:33:47 by yeblee            #+#    #+#             */
-/*   Updated: 2022/08/02 16:21:10 by yeblee           ###   ########.fr       */
+/*   Updated: 2022/08/02 16:30:34 by yeblee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,18 @@
 
 int	main(int ac, char *av[])
 {
-	
+	int		i;
+	t_stack	*a;
+	t_stack	*b;
+
+	i = 0;
+	if (ac <= 2)
+		return (0);
+	a = ps_create_stack();
+	b = ps_create_stack();
+	while (i < ac)
+		ps_parsing(av[++i], a);
+	ps_delete_stack(a);
+	ps_delete_stack(b);
 	return (0);
 }
