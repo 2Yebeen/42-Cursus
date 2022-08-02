1625d07e4aa5ca0f7058a2c586630d94bda7bc4d
yeblee <68188768+33bini@users.noreply.github.com>
2022-08-02 15:35:13 +0900

    ADD : init push_swap

---
+++ push_swap/includes/push_swap.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yeblee <yeblee@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/02 15:28:57 by yeblee            #+#    #+#             */
+/*   Updated: 2022/08/02 15:29:18 by yeblee           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+# include <stdlib.h>
+# include <unistd.h>
+# include <stdio.h>
+# include "../libft/libft.h"
+
+# define TRUE		1
+# define FALSE		0
+# define LONG_MAX 	9223372036854775807
+
+#endif
