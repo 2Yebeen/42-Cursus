7817da63fcd027e4d32827bdb39e1bc3b5266047
yeblee <68188768+33bini@users.noreply.github.com>
2022-08-03 12:33:57 +0900

    ADD : common define header

---
+++ push_swap_/includes/common_ps_def.h
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   common_ps_def.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yeblee <yeblee@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/03 11:05:22 by yeblee            #+#    #+#             */
+/*   Updated: 2022/08/03 11:37:50 by yeblee           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef COMMON_PS_DEF_H
+# define COMMON_PS_DEF_H
+
+# define FALSE	0
+# define TRUE	1
+
+# define SA		11
+# define SB		12
+# define SS		13
+
+# define PA		21
+# define PB		22
+# define RA		31
+
+# define RB		32
+# define RR		33
+
+# define RRA	41
+# define RRB	42
+# define RRR	43
+
+#endif
--- push_swap_/includes/push_swap.h
+++ push_swap_/includes/push_swap.h
@@ -3,30 +3,20 @@
 /*                                                        :::      ::::::::   */
 /*   push_swap.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: yeblee <yeblee@student.42seoul.kr>         +#+  +:+       +#+        */
+/*   By: yeblee <yeblee@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/02 22:38:30 by yeblee            #+#    #+#             */
-/*   Updated: 2022/08/03 00:02:17 by yeblee           ###   ########.fr       */
+/*   Updated: 2022/08/03 12:30:09 by yeblee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PUSH_SWAP_H
 # define PUSH_SWAP_H
 
-# define SA 11
-# define SB 12
-# define SS 13
-
-# define PA 21
-# define PB 22
-
-# define RA 31
-# define RB 32
-# define RR 33
-
-# define RRA 41
-# define RRB 42
-# define RRR 43
+# include <unistd.h>
+# include <stdlib.h>
+# include "./common_ps_def.h"
+# include "../libft/libft.h"
 
 typedef struct s_node
 {
@@ -40,5 +30,36 @@ typedef struct s_ps
 	int		count;
 	t_node	*head;
 }			t_ps;
+
+/* push_swap.c */
+void	ps_swap(t_ps *ps, int type);
+void	ps_push(t_ps *a, t_ps *b, int type);
+void	ps_rotate(t_ps *ps, int type);
+void	ps_reverse_rotate(t_ps *ps, int type);
+
+/* push_swap_utils.c */
+t_ps	*ps_create(void);
+int		ps_add_data(t_ps *ps, int data);
+t_node	*ps_get_node(t_ps *ps, int position);
+t_node	*ps_pop_top(t_ps *ps);
+t_node	*ps_pop_bottom(t_ps *ps);
+
+/* ps_utils.c */
+int		ps_atoi(const char *str);
+void	ps_free(char **str);
+void	ps_error(int fd);
+void	ps_parsing(const char *str, t_ps *a);
+void	ps_display(int type);
+
+/* ps_arr.c */
+int		*ps_arr(t_ps *ps);
+int		*ps_add_arr(t_ps *ps, int len);
+int		ps_sorting_arr(int *arr, int len);
+
+/* ps_sorting.c */
+void	ps_init_sort(t_ps *a, t_ps *b, int *sorted);
+void	ps_sort2(t_ps *a);
+void	ps_sort3(t_ps *a);
+void	ps_sorting(t_ps *a, t_ps *b, int *sotred);
 
 #endif
